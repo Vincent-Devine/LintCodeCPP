b60b5f9fab2c655eabf4b0e2bf09244c535625b4
Vincent DEVINE <v.devine@student.isartdigital.com>
2025-11-26 14:42:03 +0100

    UPDATE main

--- LintCodeCPP.cpp
+++ LintCodeCPP.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 int AddNumber(int a, int b)
 {
@@ -11,9 +12,16 @@ void SomeFunction()
     std::cout << "some function" << std::endl;
 }
 
+void PrintString(std::string s)
+{
+    std::cout << s << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello World!" << std::endl;
     std::cout << AddNumber(1, 1) << std::endl;
+    std::string s = "some string";
+    PrintString(s);
     return 0;
 }
