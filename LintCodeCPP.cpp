ecf822f94033bd1ef54e5be2f5f51a230137729f
Vincent DEVINE <v.devine@student.isartdigital.com>
2025-11-26 11:10:48 +0100

    ADD base project

---
+++ LintCodeCPP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << Add(1, 1) << std::endl;
+    return 0;
+}
